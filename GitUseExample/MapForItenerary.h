cabd965c8671a6349dc4a435f165cbf3823190aa
Lailei Huang (Alex) <laileihuang@gmail.com>
2014-07-17 15:28:05 +1000

    Creates new classes for map itinerary features

---
+++ GitUseExample/MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.h
+//  GitUseExample
+//
+//  Created by Lailei Huang on 17/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItenerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.m
+//  GitUseExample
+//
+//  Created by Lailei Huang on 17/07/2014.
+//  Copyright (c) 2014 alex.huang. All rights reserved.
+//
+
+#import "MapForItenerary.h"
+
+@implementation MapForItenerary
+
+@end
